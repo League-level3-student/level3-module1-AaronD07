728cfb9e2e90e5d840043d66842a321961db0b3a
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2020-05-01 15:29:19 -0700

    :smile:

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,10 +6,34 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+ArrayList<String> list = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		list.add("Hello");
+		list.add("there");
+		list.add("I");
+		list.add("am");
+		list.add("Aaron");
 		//3. Print all the Strings using a standard for-loop
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		for(String s:list) {
+			System.out.println(s);
+			
+		}
+		for(int i=0; i<list.size(); i++) {
+			if (i%2==0) {
+				System.out.println(list.get(i));
+			}
+			
+		}
+		
+	
+			for (int i=list.size();i > 1; i--) {
+
+				System.out.println(list.get(i));
+			}
+		
 		
 		
 		//4. Print all the Strings using a for-each loop
