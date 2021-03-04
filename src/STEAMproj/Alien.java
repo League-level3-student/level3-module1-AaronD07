03dafd1f23ff5af0e3563b5c7d73dd47ab01f0b0
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2021-03-04 08:40:18 -0800

    I will delete later

--- src/STEAMproj/Alien.java
+++ src/STEAMproj/Alien.java
@@ -1,45 +1,45 @@
-//package STEAMproj;
-//import java.awt.Color;
-//import java.awt.Graphics;
-//import java.awt.image.BufferedImage;
+package STEAMproj;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
-//import javax.imageio.ImageIO;
+import javax.imageio.ImageIO;
 
 
 
-//public class Alien extends GameObject{
-//	public static BufferedImage image;
-//	public static boolean needImage = true;
-//	public static boolean gotImage = false;	
-//	public Alien(int x, int y, int width, int height) {
-//		super(x, y, width, height, 1);
-//		if (needImage) {
-//		    loadImage ("alien.png");
-//		}
-//	}
-//	public void update() {
-//		y+=speed;
-//		   super.update();
-//	}
-//	public void draw(Graphics g) {
-//		  g.setColor(Color.YELLOW);
-//	        g.fillRect(x, y, width, height);
-//	        if (gotImage) {
-//	        	g.drawImage(image, x, y, width, height, null);
-//	        } else {
-//	        	g.setColor(Color.BLUE);
-	    //    	g.fillRect(x, y, width, height);
-	  //      }
-	//}
-//	void loadImage(String imageFile) {
-	    //if (needImage) {
-	  //      try {
-	//            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
-//		    gotImage = true;
-//	        } catch (Exception e) {
-//	            
-//	        }
-//	        needImage = false;
-//	    }
-//	}
-//}
+public class Alien extends GameObject{
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;	
+	public Alien(int x, int y, int width, int height) {
+		super(x, y, width, height, 1);
+		if (needImage) {
+		    loadImage ("alien.png");
+		}
+	}
+	public void update() {
+		y+=speed;
+		   super.update();
+	}
+	public void draw(Graphics g) {
+		  g.setColor(Color.YELLOW);
+	        g.fillRect(x, y, width, height);
+	        if (gotImage) {
+	        	g.drawImage(image, x, y, width, height, null);
+        } else {
+	        	g.setColor(Color.BLUE);
+	        	g.fillRect(x, y, width, height);
+	        }
+	}
+	void loadImage(String imageFile) {
+	    if (needImage) {
+	        try {
+	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+		    gotImage = true;
+	        } catch (Exception e) {
+	            
+	        }
+	        needImage = false;
+    }
+	}
+}
